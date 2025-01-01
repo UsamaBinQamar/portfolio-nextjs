c6d01379967af46781975e45d6b902064ef829a9
UsamaBinQamar <usamadevio@gmail.com>
2025-01-02 01:57:40 +0500

    add personal content

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,9 +5,9 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I&#39;m Hamed.</h1>
+        <h1 className='title no-underline'>Hey, I&#39;m Usama.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;m a software engineer based in Toronto, Canada. I&#39;m
+          I&#39;m a software engineer based in Lahore, Pakistan. I&#39;m
           passionate about learning new technologies and sharing knowledge with
           others.
         </p>
@@ -16,7 +16,7 @@ export default function Intro() {
         <Image
           className='flex-1 rounded-lg grayscale'
           src={authorImage}
-          alt='Hamed Bahram'
+          alt='Hafiz Syed Usama Bin Qamar'
           width={175}
           height={175}
           priority
