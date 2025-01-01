074500862029d9badec4ab7b5f7853f76df9e3bf
UsamaBinQamar <usamadevio@gmail.com>
2025-01-01 23:54:09 +0500

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-5xl font-bold">Portfolio</h1>
+      </div>
+    </section>
+  );
+};
