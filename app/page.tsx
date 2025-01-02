f3338229c2fc3ba3fc33cfee43b8a3c72aa687e7
UsamaBinQamar <usamadevio@gmail.com>
2025-01-03 01:12:24 +0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Intro from '@/components/intro'   
+import { MDXRemote } from 'next-mdx-remote/rsc'
 
 const page = () => {
+  const content = `
+  #  I'm a software engineer based in Lahore, Pakistan. I'm passionate about learning new technologies and sharing knowledge with others.
+  `
   return (
     <section className="py-24">
     <div className="container max-w-3xl">
      <Intro/>
+
+     <MDXRemote source={content} />
      </div>
   </section>
   )
---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+// const page = async({params}:{params:{slug:string}}) => {
+//     const {slug} = params
+//     const content = await getPostBySlug(slug)
+//     return {props:{content}}
+//   return (
+//     <div>page</div>
+//   )
+// }
+
+// export default page
